e75eae8cab7f844b7b8405b97aa3da02527d8a3e
Keith Millar <keithmillar@hotmail.ca>
2020-06-02 02:09:07 +0000

    updated tests to mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,24 +1,21 @@
-const assertArraysEqual = require('../assertArraysEqual');
-// const eqArrays = require('./eqArrays');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-// assertArraysEqual(middle([1, 2, 3, 7, 4, 5, 6]), [3, 4]);
-
-// // TEST CODE for middle()
-// console.log(middle([1])) // => []
-// console.log(middle([1, 2])) // => []
-
-// console.log(middle([1, 2, 3])) // => [2]
-// console.log(middle([1, 2, 3, 4, 5])) // => [3]
-
-// console.log(middle([1, 2, 3, 4])) // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+describe("#middle", () => {
+  it("returns [] for [1] because no middle", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [1, 2] because no middle", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [2] for [1, 2, 3] because odd number array length", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5] because odd number array length", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4] because array length even, middle is shared", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,32 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it("ensures we get back the correct number of elements", () => {
+    const arr = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(arr).length, arr.length - 1);
+  });
+  it("ensures we get back two elements", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+  it("ensures we get back Lighthouse and Labs as the last two elements of the array", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns Labs as it should be the value of index one of the new array", () => {
+    const arr = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(arr)[1], "Labs");
+  });
+  it("An array of one element should be left empty", () => {
+    const arr = ["hello"];
+    assert.deepEqual(tail(arr), []);
+  });
+  it("An empty array should yield an empty array for its tail", () => {
+    const arr = [];
+    assert.deepEqual(tail(arr), []);
+  });
+});
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// More edge case tests
-const arrayOfOne = tail(["Hello"]);
-assertEqual(arrayOfOne.length, 0); // An array with only one element should yield an empty array for its tail
-
-const arrayOfNone = tail([]);
-assertEqual(arrayOfNone.length, 0); // An empty array should yield an empty array for its tail
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
